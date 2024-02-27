570f4f027761faef26b8e508f05337bcac72efb6
yenorii <144069387+Yenorii@users.noreply.github.com>
2024-02-26 22:51:24 -0500

    adding starting files

---
+++ SurfsUp/app.py
@@ -0,0 +1,19 @@
+# Import the dependencies.
+from flask import Flask, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+
+#################################################
+# Database Setup
+#################################################
+
+#################################################
+# Flask Setup
+#################################################
+
+#################################################
+# Flask Routes
+#################################################
+
+
